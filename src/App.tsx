739e89103214f9cb7a4b2607b726b76cc6921422
lucianbc <lucianboaca97@gmail.com>
2020-11-09 19:15:23 +0000

    Moved realm provider up

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { Root } from 'native-base';
 import React from 'react';
+import RealmProvider from './components/RealmProvider';
 import TabNavigator from './navigation/Tabs';
 
 const App = () => {
   return (
     <Root>
-      <NavigationContainer>
-        <TabNavigator />
-      </NavigationContainer>
+      <RealmProvider>
+        <NavigationContainer>
+          <TabNavigator />
+        </NavigationContainer>
+      </RealmProvider>
     </Root>
   );
 };
--- src/exercises/index.tsx
+++ src/exercises/index.tsx
@@ -2,25 +2,22 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as Exercises from './ExercisesScreen';
 import * as AddExercise from './AddExerciseScreen';
-import RealmProvider from '../components/RealmProvider';
 
 const Stack = createStackNavigator();
 
 const Navigator = () => (
-  <RealmProvider>
-    <Stack.Navigator>
-      <Stack.Screen
-        name="exercises-index"
-        component={Exercises.default}
-        options={Exercises.options}
-      />
-      <Stack.Screen
-        name="exercises-add-exercise"
-        component={AddExercise.default}
-        options={AddExercise.options}
-      />
-    </Stack.Navigator>
-  </RealmProvider>
+  <Stack.Navigator>
+    <Stack.Screen
+      name="exercises-index"
+      component={Exercises.default}
+      options={Exercises.options}
+    />
+    <Stack.Screen
+      name="exercises-add-exercise"
+      component={AddExercise.default}
+      options={AddExercise.options}
+    />
+  </Stack.Navigator>
 );
 
 export default Navigator;
